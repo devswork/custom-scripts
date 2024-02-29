1d832339a2be5925f45d5e075e9ee6d826e38eab
tian <devswork@163.com>
2024-02-29 10:55:03 +0800

    update

---
+++ windows/active_office_LTSC_2021_Standard.bat
@@ -0,0 +1,6 @@
+cd "C:\Program Files\Microsoft Office\Office16"
+cscript ospp.vbs /inpkey:KDX7X-BNVR8-TXXGX-4Q7Y8-78VT3
+cscript ospp.vbs /sethst:10.10.100.2
+cscript ospp.vbs /act
+pause
+
---
+++ windows/active_windows.bat
@@ -0,0 +1,5 @@
+slmgr /ipk W269N-WFGWX-YVC9B-4J6C9-T83GX
+slmgr /skms 10.10.100.2
+slmgr /ato
+
+pause
